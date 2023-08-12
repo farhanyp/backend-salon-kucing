5d8d039f3ee19a8885fe9cbc36586724839352e5
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 16:10:59 +0700

    aa

--- src/application/app.js
+++ src/application/app.js
@@ -1,33 +1,33 @@
 import express from 'express'
-import mongoose from "mongoose"
-import { publicApi } from "../routes/public-api.js";
-import { apiRouter } from "../routes/api.js";
-import { errorMiddleware } from '../middleware/error-middleware.js'
+// import mongoose from "mongoose"
+// import { publicApi } from "../routes/public-api.js";
+// import { apiRouter } from "../routes/api.js";
+// import { errorMiddleware } from '../middleware/error-middleware.js'
 
 export const app = express();
-mongoose.connect('mongodb://localhost:27017/db_salon_hewan', { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err)
-    });
+// mongoose.connect('mongodb://localhost:27017/db_salon_hewan', { useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => {
+//         console.log('Connected to MongoDB')
+//     })
+//     .catch((err) => {
+//         console.error('Error connecting to MongoDB:', err)
+//     });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
   
 app.get('/', (req, res) => {
-  res.send('public api!')
+  res.send("hello")
 })
 
-app.use("/api/v1/member",publicApi)
+// app.use("/api/v1/member",publicApi)
 // app.use("/api/v1/admin",apiRouter)
 
 
