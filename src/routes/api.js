b4d0d676d5782932e5ce20526779c2b220120a26
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 16:45:43 +0700

    aa

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,27 +7,27 @@ import lodgingController from '../controller/lodgingController.js'
 
 const apiRouter = new express.Router()
 
-apiRouter.use(authMiddleware)
-apiRouter.post('/category/create', categoryController.create)
-apiRouter.get('/categories', categoryController.get)
-apiRouter.patch('/category/:categoryId', categoryController.update)
-apiRouter.delete('/category/:categoryId', categoryController.remove)
+// apiRouter.use(authMiddleware)
+// apiRouter.post('/category/create', categoryController.create)
+// apiRouter.get('/categories', categoryController.get)
+// apiRouter.patch('/category/:categoryId', categoryController.update)
+// apiRouter.delete('/category/:categoryId', categoryController.remove)
 
 
-apiRouter.post('/product/create', productController.create)
-apiRouter.get('/product', productController.get)
-apiRouter.patch('/product/:productId', productController.update)
-apiRouter.delete('/product/:productId', productController.remove)
+// apiRouter.post('/product/create', productController.create)
+// apiRouter.get('/product', productController.get)
+// apiRouter.patch('/product/:productId', productController.update)
+// apiRouter.delete('/product/:productId', productController.remove)
 
 
-apiRouter.post('/booking/times', availableTimesController.create)
-apiRouter.get('/booking/times', availableTimesController.get)
+// apiRouter.post('/booking/times', availableTimesController.create)
+// apiRouter.get('/booking/times', availableTimesController.get)
 
 
-apiRouter.post('/lodging/create', lodgingController.create)
-apiRouter.get('/lodging', lodgingController.get)
-apiRouter.patch('/lodging/:lodgingId', lodgingController.update)
-apiRouter.delete('/lodging/:lodgingId', lodgingController.remove)
+// apiRouter.post('/lodging/create', lodgingController.create)
+// apiRouter.get('/lodging', lodgingController.get)
+// apiRouter.patch('/lodging/:lodgingId', lodgingController.update)
+// apiRouter.delete('/lodging/:lodgingId', lodgingController.remove)
 
 
 export {
