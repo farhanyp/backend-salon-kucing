e99583f296d82953f51b3479a7d9f35ca19560ed
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 13:41:04 +0700

    fix

--- src/application/app.js
+++ src/application/app.js
@@ -14,14 +14,14 @@ mongoose.connect('mongodb://localhost:27017/db_salon_hewan', { useNewUrlParser: 
     });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
   
 
 // app.use(publicApi)
