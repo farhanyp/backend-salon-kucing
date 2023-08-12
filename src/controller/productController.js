61092454167dda072dd23f1b52f456678f69e0da
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 16:24:00 +0700

    aaa

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -20,7 +20,7 @@ const create = async (req, res, next) => {
 const get = async (req, res, next) => {
     try{
 
-        const result = await productService.get()
+        // const result = await productService.get()
 
         res.status(200).json({
             data: "result"
