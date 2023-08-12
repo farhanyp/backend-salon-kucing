ca025ff73fc275227aadad7e8563dbb014b47755
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 16:12:11 +0700

    aaa

--- src/application/app.js
+++ src/application/app.js
@@ -16,12 +16,12 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti '*' dengan domain yang sesuai di produksi
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
   
 app.get('/', (req, res) => {
   res.send("hello")
