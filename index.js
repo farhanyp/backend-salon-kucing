1105d8a70f5db979561245f7bba2cb32bcc6ed35
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 14:05:50 +0700

    fix

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import express from 'express'
-import mongoose from "mongoose"
-// import { app } from "./src/application/app.js";
-import { publicApi } from "./src/routes/public-api.js";
-import { apiRouter } from "./src/routes/api.js";
+import { app } from "./src/application/app.js";
 
-const app = express();
-// mongoose.connect('mongodb://localhost:27017/db_salon_hewan', { useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => {
-//         console.log('Connected to MongoDB')
-//     })
-//     .catch((err) => {
-//         console.error('Error connecting to MongoDB:', err)
-//     });
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!aa')
-})
-
-// app.use(publicApi)
-// app.use(apiRouter)
 
 app.listen(3000, () => console.log('Sever Running On port 3000'))
 
