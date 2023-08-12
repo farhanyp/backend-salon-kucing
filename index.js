15abcfef9434b19246cfa7ae09e2db56ba401385
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 13:29:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import { app } from "./src/application/app.js";
+// import { app } from "./src/application/app.js";
+import express from 'express'
 
+const app = express()
+const publicApi = new express.Router()
 
+app.use(express.json());
+
+app.use(publicApi)
+
+publicApi.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(3000, () => console.log('Sever Running On port 3000'))
 
