5372decd8449c80177964dfcf67fcc17058cd3c2
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-12 16:53:02 +0700

    fix lodging controller

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,7 @@ import categoryController from '../controller/categoryController.js'
 import { authMiddleware } from '../middleware/auth-middleware.js'
 import productController from '../controller/productController.js'
 import availableTimesController from '../controller/availableTimesController.js'
-// import lodgingController from '../controller/lodgingController.js'
+import lodgingController from '../controller/lodgingController.js'
 
 const apiRouter = new express.Router()
 
@@ -24,10 +24,10 @@ apiRouter.post('/booking/times', availableTimesController.create)
 apiRouter.get('/booking/times', availableTimesController.get)
 
 
-// apiRouter.post('/lodging/create', lodgingController.create)
-// apiRouter.get('/lodging', lodgingController.get)
-// apiRouter.patch('/lodging/:lodgingId', lodgingController.update)
-// apiRouter.delete('/lodging/:lodgingId', lodgingController.remove)
+apiRouter.post('/lodging/create', lodgingController.create)
+apiRouter.get('/lodging', lodgingController.get)
+apiRouter.patch('/lodging/:lodgingId', lodgingController.update)
+apiRouter.delete('/lodging/:lodgingId', lodgingController.remove)
 
 
 export {
