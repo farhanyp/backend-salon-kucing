5acbfca8a83d6155e18e4af019d39072b566251e
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-08-19 17:11:41 +0700

    try fix server

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -18,10 +18,10 @@ adminRouter.patch('/admin/category', adminController.editCategory)
 adminRouter.delete('/admin/category/:categoryId', adminController.deleteCategory)
 
 
-adminRouter.get('/admin/product', adminController.viewProduct)
-adminRouter.post('/admin/product/create', uploadSingle, adminController.addProduct)
-adminRouter.patch('/admin/product', uploadSingle, adminController.editProduct)
-adminRouter.delete('/admin/product/:productId', adminController.deleteProduct)
+// adminRouter.get('/admin/product', adminController.viewProduct)
+// adminRouter.post('/admin/product/create', uploadSingle, adminController.addProduct)
+// adminRouter.patch('/admin/product', uploadSingle, adminController.editProduct)
+// adminRouter.delete('/admin/product/:productId', adminController.deleteProduct)
 
 
 adminRouter.get('/admin/available-times', adminController.viewAvailableTimes)
